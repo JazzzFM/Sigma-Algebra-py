2744f481f32ea543dfc346e02df3bf27203df40b
root <jazzzzfm@protonmail.com>
2021-04-24 11:48:27 -0500

    Se sube ultima act

--- OperacionesConjuntos.py
+++ OperacionesConjuntos.py
@@ -54,41 +54,56 @@ def Obtener_Complemento(lista, lista_elemento):
 def Criterio_Complemento(lista):
     complementos = []
     aux = [] 
-    
     for i in range(0, len(lista)):
         aux.append(lista[i])
-    
     Conjunto = max(lista, key = lambda i: len(i))
-    
     for a in aux:
         complementos.append(Obtener_Complemento(Conjunto, a))            
     verdades = []
-
     for i in range(0, len(complementos)):
         if complementos[i] in lista:
             verdades.append(True)
         else:
             verdades.append(False)
-    
     return all(verdades)
 
 def Criterio_Union(lista):
-    pass
+    uniones = []
+    for i in range(0, len(lista)):
+        for j in range(0, i):
+            aux_1 = copy.copy(lista[j])
+            aux_2 = copy.copy(lista[i])
+            aux = Union(aux_1, aux_2)
+            uniones.append(aux)
+    verdades = []
+    for i in range(0, len(uniones)):
+        print(uniones[i])
+        if uniones[i] in lista:
+            print(True)
+            verdades.append(True)
+        else:
+            print(False)
+            verdades.append(False)
+    print(verdades)
+    return all(verdades)
 
 def Es_SigmaAlgebra(lista):
     if not Vacio_Esta_En(lista):
         print("No es sigma-álgebra, pues el vacío no está en en la familia. \n")
         return False
     else:
-        return Criterio_Complemento(lista)      
-        
-        #else:
-        #   print("No es sigma-álgebra, pues no cumple el criterio de que cada elemento de la familia, se complemento está en la familia. \n")
-        #    False
-    
+        if Criterio_Complemento(lista) and Criterio_Union(lista):
+            return True
+        else:
+            if not Criterio_Complemento(lista):
+                print("No es sigma-álgebra, pues no cumple el criterio de que cada elemento de la familia, su complemento está en la familia. \n")
+            if not Criterio_Union(lista):
+                print("No es sigma-álgebra, pues no cumple el criterio de que cada colección finita de la familia, su unión está en la familia. \n")
+            return False
     
 if __name__== '__main__': 
     lista = [[],['a'],['b'], ['a', 'b'], ['c'], ['a', 'c'], ['b', 'c'], ['a', 'b', 'c']];
-    x = Es_SigmaAlgebra(lista)
-    print(x) 
+    #x = Es_SigmaAlgebra(lista)
+    y = Es_SigmaAlgebra(lista)
+    print(y) 
     
