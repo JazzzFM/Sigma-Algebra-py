9ae148dd472cc0f15499cbab7e3d73f43a8d7dbc
JazzzFM <jazzzfm@protonmail.com>
2021-06-15 12:40:32 -0500

    Primer paso terminado

--- OperacionesConjuntos.py
+++ OperacionesConjuntos.py
@@ -3,7 +3,6 @@
 import math 
 import copy
  
-#######################################
 # Operaciones con conjuntos
 
 def Union(lista_1, lista_2):
@@ -63,82 +62,62 @@ def Esta_En(lista_1, lista_2):
             verdades.append(False)
     return any(verdades)
 
-#############################################
 # Criterios para sigma-álgebra
 
 def Vacio_Esta_En(lista):
     x = [True for elemento in lista if len(elemento) == 0]
     return any(x)
 
-def Criterio_Complemento(lista):
-    complementos = []
-    aux = [] 
-    for i in range(0, len(lista)):
-        aux.append(lista[i])
-
-    Total = max(lista, key = lambda i: len(i))
-    
-    for a in aux:
-        complementos.append(Obtener_Complemento(Total, a))            
-    verdades = []
-    
-    for i in range(0, len(complementos)):
-        if Esta_En(complementos[i], lista):
-            verdades.append(True)
-        else:
-            verdades.append(False)
+def Criterio_Complemento(lista,n):
+    lista_total = list(range(n))
+    complementos = [Obtener_Complemento(lista_total, a) for a in lista]
+    verdades = [Esta_En(c, lista) for c in complementos] 
     return all(verdades)
         
 def Criterio_Union(lista):
     uniones = []
-    for i in range(0, len(lista)):
-        for j in range(0, i):
+    for i in range(len(lista)):
+        for j in range(i):
             aux_1 = copy.copy(lista[j])
             aux_2 = copy.copy(lista[i])
             aux = Union(aux_1, aux_2)
             uniones.append(aux)
     verdades = []
-    for i in range(0, len(uniones)):
+    for i in range(len(uniones)):
         if Esta_En(uniones[i], lista):
             verdades.append(True)
         else:
             verdades.append(False)
     return all(verdades)
 
-def Es_SigmaAlgebra(lista):
+def Es_SigmaAlgebra(lista, n):
     if not Vacio_Esta_En(lista):
-        print("No es sigma-álgebra, pues el vacío no está en en la familia. \n")
+        None
         return False
     else:
-        if Criterio_Complemento(lista) and Criterio_Union(lista):
+        if Criterio_Complemento(lista, n) and Criterio_Union(lista):
             return True
         else:
-            if not Criterio_Complemento(lista):
-                print("No es sigma-álgebra, pues no cumple el criterio de que cada elemento de la familia, su complemento está en la familia. \n")
-            if not Criterio_Union(lista):
-                print("No es sigma-álgebra, pues no cumple el criterio de que cada colección finita de la familia, su unión está en la familia. \n")
+            if not Criterio_Complemento(lista, n):
+                None
+            if  not Criterio_Union(lista):
+                None
             return False
         
-###################################
 #  Construir todas las álgebras sobre el conjunto {0,1,...,n-1} donde n es un número dado pequeño.
 
 def Construir_SigmaAlgebras(n):
-    SigmaAlgebras = []
-    conjunto_inicial = [i for i in range(0, n)]
+    conjunto_inicial = list(range(n))
     Potencia = Potencia_De(conjunto_inicial)
     Potencia_De_Potencia = Potencia_De(Potencia)      
-    
-    
-    for lista in Potencia_De_Potencia:
-        if Es_SigmaAlgebra(lista):
-            SigmaAlgebras.append(lista)
-        else:
-            None
+    SigmaAlgebras = [j for j in Potencia_De_Potencia if Es_SigmaAlgebra(j, n) ]
     
     for sigma in SigmaAlgebras:
         print(sigma)
         print("\n")
         
+    print("El numero de sigma algebras son: ")
+    print(len(SigmaAlgebras))
+    print("Cantidadd de elementos en el conjunto potencia del conjunto potencia:")
+    print(len(Potencia_De_Potencia))
     return SigmaAlgebras
-
-    
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 from OperacionesConjuntos import *
 from contextlib import redirect_stdout
+import time
 
 if __name__== '__main__': 
     print("Programa que dado un n, construye todas las álgebras sobre el conjunto {0,1,...,n-1} donde n es un número dado pequeño. \n")
@@ -10,11 +11,12 @@ if __name__== '__main__':
     input_a = input()
     input_a = int(input_a)
     print("\n")
-        
+
+    inicio=time.time()    
     a = Construir_SigmaAlgebras(input_a)
+    fin=time.time()
+    print("\n")
+    print("Tiempo de ejecucion:\n")
+    print(fin-inicio)
+    print("\n")
     print(a)
-    
-    
-    #with open('out.txt', 'x') as f:
-    #    with redirect_stdout(f):
-    #        print(a)
